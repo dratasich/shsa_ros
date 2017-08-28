20580c6a1d2fb5d9ad1cec69db2863f9a5a68277
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2017-08-28 15:32:32 +0200

    add interface to shsa_node (working)
    * add action server
    * add topic to variable mapping (ROS to model translation)
    * change package name 'shsa' to 'shsa_ros' because 'shsa' conflicts with the
      shsa library loaded (e.g., shsa.msg vs. shsa.engine.greedy)
    * add configuration possibility via private parameters

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# SHSA Wrapper for ROS
+# ROS Wrapper for SHSA
 Self-Healing by Structural Adaptation (SHSA) for the Robot Operating System
 (ROS).
+
+
+## Dependencies
+
+* create a symlink `shsa` in the `src` directory to
+  the [shsa library](https://github.com/dratasich/shsa)
+* install `rospkg` for python3
+    ```bash
+    pip3 install --user rospkg
+    ```
+
+## Run
+
+Execute SHSA node:
+```bash
+rosrun shsa_ros shsa_node.py _model:=/path/to/shsa-model.yaml _map:=/path/to/variable-topic-mapping.yaml &
+```
+
+Substitute a topic (send a goal, i.e., trigger an action of the SHSA node):
+```bash
+rostopic pub /substitute/goal shsa_ros/SubstituteActionGoal
+```
+
+Trigger auto-completion by pressing `tab` two times, the content of the goal
+message can be filled (`goal.topic`).
--- src/shsa_node.py
+++ src/shsa_node.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 """SHSA node.
 
 __author__ = Denise Ratasich
@@ -6,44 +6,108 @@ __date__ = 2017-08-25
 
 ROS wrapper for self-healing by structural adaptation.
 
-usage: shsa_node.py [-h]
+Set parameters ~model (SHSA model) and ~map (variable to topic mapping).
 
-optional arguments:
-    -h, --help            show this help message and exit
 """
+
+import sys
+import argparse
+import yaml
 
 import roslib
 import rospy
-import rostopic
-import sys
-import argparse
+import actionlib
+import shsa_ros.msg
+
+# there should be a more appropriate way of including the shsa library
+sys.path.insert(0, "/home/denise/repos/work/research/shsa/shsa/")
+from engine.greedy import Greedy
+
 
 _node_name = 'shsa_node'
 """Name of this node in the ROS system."""
 
 
-def _extract_args():
-    # get the arguments for this node (no ROS params or remappings)
-    argv = rospy.myargv(argv=sys.argv)
-    argv.pop(0)  # remove first element, not needed for args parsing
-    rospy.logdebug("Arguments list (stripped): %s", argv)
-    # parse args
-    parser = argparse.ArgumentParser()
-    # todo
-    args = parser.parse_args(argv)
-    return args
+class Map(object):
+    """Translates topic to variable (ROS to model), and vice-versa."""
+    def __init__(self, configfile):
+        self.__configfile = configfile
+        self.__data = None
+        # open config and read yaml
+        with open(configfile, 'r') as f:
+            try:
+                self.__data = yaml.load(f)
+                datacpy = dict(self.__data)
+            except yaml.YAMLError as e:
+                raise RuntimeError("""Failed to read map config
+                ({}). {}""".format(configfile, e))
+        # make map bi-directional (reversed duplicate)
+        for k,v in datacpy.items():
+            self.__data[v] = k
 
-def _setup_action_server():
-    # todo
-    pass
+    def topic(self, variable):
+        return self.__data[variable]
+
+    def variable(self, topic):
+        return self.__data[topic]
+
+
+class SubstituteServer(object):
+    def __init__(self, modelfile, mapfile):
+        self.__engine = Greedy(configfile=modelfile)
+        """SHSA engine."""
+        self.__map = Map(mapfile)
+        """Mapping ROS topics to SHSA model variables."""
+        self.__tnn = 0
+        """ID of next transfer node, or number of transfer nodes created."""
+        self.__server = actionlib.SimpleActionServer('substitute',
+                                                     shsa_ros.msg.SubstituteAction,
+                                                     self.execute, False)
+        self.__server.start()
+        # prepare result message
+        self.__result = shsa_ros.msg.SubstituteResult()
+        """Result message, if the substitution succeeds."""
+
+    def __start_transfer_node(self, s):
+        if s is None or len(s) == 0:
+            raise RuntimeError("""Substitution result empty.""")
+        rospy.logdebug("create transfer node for {}".format(s))
+        # todo: gather input, code, output
+        # cmd
+        node_name = "transfer_node_{}".format(self.__tnn)
+        cmd = "rosrun dynamic_reconfiguration transfer_function_node.py"
+        # special keyword/argument for ros to set node name
+        cmd += " __name:={}".format(node_name)
+        # 3 positional arguments for x, code and y
+	# cmd += " -i {}".format("x")
+	# cmd += " -c {}".format("y=x")
+	# cmd += " -o {}".format("y")
+        # todo: start node
+        self.__tnn += 1
+        return node_name
+
+    def execute(self, goal):
+        rospy.loginfo("Substitution of '{}' requested.".format(goal.topic))
+        try:
+            variable = self.__map.variable(goal.topic)
+            s = self.__engine.substitute(variable)
+            self.__result.node = self.__start_transfer_node(s.best())
+            rospy.loginfo("Substitution of '{}' succeeded.".format(goal.topic))
+            self.__server.set_succeeded(self.__result)
+        except Exception as e:
+            rospy.logwarn("Substitution of '{}' failed. {}".format(goal.topic,
+                                                                   e))
+            self.__server.set_aborted()
+
 
 # main entry point of this node
 if __name__ == '__main__':
     try:
         # setup
         rospy.init_node(_node_name)
-        args = _extract_args()
-        _setup_action_server()
+        model = rospy.get_param('~model')
+        vmap = rospy.get_param('~map')
+        aserver = SubstituteServer(model, vmap)
         # loop
         rospy.spin()
     except Exception as e:
